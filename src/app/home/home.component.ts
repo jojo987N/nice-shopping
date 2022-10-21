ca47f7df125e0d50599d83476c54b0f94bf10b2f
nass <nass@gmail.com>
2022-10-21 22:46:20 +0200

    fh

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  title = 'Home page';
+  title = '<b>Home page</b>';
   description!:string;
   constructor() { }
 
