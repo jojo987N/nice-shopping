3bce16ae192507fe4945618af02c43c2f1d04ec1
nass <nass@gmail.com>
2022-10-21 20:21:56 +0200

    dg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: '<h1>Bonjour</h1>',
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
 
-    
+
   }
 
 }
