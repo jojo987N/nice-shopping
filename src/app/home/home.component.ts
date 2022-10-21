51c8fc81ee5d8bb4f5eee0f963da23feca2c5009
nass <nass@gmail.com>
2022-10-21 15:55:06 +0200

    fthet

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: '<h1>Bonjour</h1>',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
